7f405af97db48732a66d5df0ff9df7599d469892
harvli <hl.138@hotmail.com>
2023-01-16 19:41:40 -0500

    comments for sliding window

--- Problems/sliding_window.js
+++ Problems/sliding_window.js
@@ -1,13 +1,13 @@
 const max_sub_array_of_size_k = function (k, arr) {
-	let windowSum = 0,
+	let windowSum = 0, // keep track of sum
 		maxSum = 0,
-		windowStart = 0;
+		windowStart = 0; // starting point of window
 	for (let windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-		windowSum += arr[windowEnd];
-		if (windowEnd >= k - 1) {
-			maxSum = Math.max(windowSum, maxSum); // keep track of max sum
-			windowSum -= arr[windowStart]; // subtract window start element
-			windowStart++;
+		windowSum += arr[windowEnd]; // increment sum with next val
+		if (windowEnd >= k - 1) { // if the end of window is greater than the given size of k
+			maxSum = Math.max(windowSum, maxSum); // reassign maxSum to the greater sum
+			windowSum -= arr[windowStart]; // subtract window start element from sum
+			windowStart++; // shift window to next index
 		}
 	}
 	return maxSum;
